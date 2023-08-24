209dcea153f4e67818acaf486be4fb3ead77c324
pkulik0 <pkulik0@icloud.com>
2023-08-24 22:43:42 +0200

    App: add navbar

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -1,2 +1,16 @@
-Layout
-<slot/>
+<script>
+import LanguageDropdown from "./LanguageDropdown.svelte";
+</script>
+
+<nav class="navbar navbar-expand-lg bg-primary mb-2">
+    <div class="container-fluid">
+        <div class="navbar-brand">AutoCC</div>
+        <div class="navbar-nav">
+            <LanguageDropdown/>
+        </div>
+    </div>
+</nav>
+
+<div class="container-fluid">
+    <slot/>
+</div>
