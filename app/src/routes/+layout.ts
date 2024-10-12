098a64abf34468cb3adad940483980313b83be5c
pkulik0 <70904851+pkulik0@users.noreply.github.com>
2024-10-12 04:23:01 +0000

    Setup app project, add navbar

--- app/src/routes/+layout.ts
+++ app/src/routes/+layout.ts
@@ -1,1 +1,10 @@
 export const prerender = true
+
+import { register, init, getLocaleFromNavigator } from 'svelte-i18n';
+
+register('en', () => import('$lib/locales/en.json'));
+
+init({
+	fallbackLocale: 'en',
+	initialLocale: getLocaleFromNavigator(),
+})
---
+++ app/src/routes/Navbar.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, Button } from 'flowbite-svelte';
+	
+	import { _ } from 'svelte-i18n';
+
+	$: activeUrl = $page.url.pathname;
+</script>
+
+<Navbar>
+	<NavBrand href="/">
+		<img src="/images/autocc.svg" class="me-1 mt-0.5 h-16 sm:h-12" alt="AutoCC Logo" />
+		<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">{$_("app.name")}</span>
+	</NavBrand>
+	<div class="flex md:order-2">
+		<Button size="sm">{$_("nav.sign_in")}</Button>
+		<NavHamburger />
+	</div>
+	<NavUl {activeUrl} class="order-1">
+		<NavLi href="/">{$_("nav.home")}</NavLi>
+		<NavLi href="/credentials">{$_("nav.credentials")}</NavLi>
+		<NavLi href="/settings">{$_("nav.settings")}</NavLi>
+	</NavUl>
+</Navbar>
